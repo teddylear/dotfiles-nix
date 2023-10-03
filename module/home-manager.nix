a8bacb5e2e1663ec35c6ca506b533aad27c5cf53
teddylear <teddylear@gmail.com>
2023-10-02 22:57:46 -0400

    adding more dependencies

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -2,6 +2,19 @@
 
 {
   # add home-manager user settings here
-  home.packages = with pkgs; [ git neovim ripgrep ];
+  home.packages = with pkgs; [
+      git
+      ripgrep
+      bat
+      tmux
+      jq
+      tree
+      luajitPackages.luarocks-nix
+      delta
+      sd
+      neofetch
+      mcfly
+      zsh
+  ];
   home.stateVersion = "23.05";
 }
