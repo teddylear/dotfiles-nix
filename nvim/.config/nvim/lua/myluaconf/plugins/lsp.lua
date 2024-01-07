601dc7042ea170bc065a6672655e2bf79fa69bfc
teddylear <20077627+teddylear@users.noreply.github.com>
2024-01-06 19:34:07 -0500

    chore: format lua

--- nvim/.config/nvim/lua/myluaconf/plugins/lsp.lua
+++ nvim/.config/nvim/lua/myluaconf/plugins/lsp.lua
@@ -149,7 +149,7 @@ return {
                 desc = "vim lsp get references",
             })
 
-            local group = vim.api.nvim_create_augroup('THE_KENSTER_LSP', {})
+            local group = vim.api.nvim_create_augroup("THE_KENSTER_LSP", {})
             vim.api.nvim_create_autocmd({ "BufWritePre" }, {
                 pattern = { "*.tf", "*.tfvars" },
                 group = group,
