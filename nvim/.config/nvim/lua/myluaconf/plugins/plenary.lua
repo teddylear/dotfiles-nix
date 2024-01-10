4b9e3f67c113c000f559b628b5a63f06010731cb
teddylear <20077627+teddylear@users.noreply.github.com>
2024-01-09 21:13:36 -0500

    fix: make screenShare command work

--- nvim/.config/nvim/lua/myluaconf/plugins/plenary.lua
+++ nvim/.config/nvim/lua/myluaconf/plugins/plenary.lua
@@ -80,7 +80,7 @@ return {
         local function screenShare()
             local home_dir = vim.env.HOME
             local alacritty_config_path_string =
-                "/shell_config/alacritty/.config/alacritty/alacritty.yml"
+                "/dotfiles-nix/alacritty/.config/alacritty/alacritty.yml"
             local alacritty_config_path = Path:new(
                 string.format("%s%s", home_dir, alacritty_config_path_string)
             )
@@ -103,11 +103,11 @@ return {
             -- size value is always second value
             local current_size = tonumber(t[2])
             print("current_size:", current_size)
-            if current_size == 20 then
+            if current_size == 18 then
                 alacritty_file_contents[size_line_index] = "  size: 25.0"
                 vim.wo.relativenumber = false
             else
-                alacritty_file_contents[size_line_index] = "  size: 20.0"
+                alacritty_file_contents[size_line_index] = "  size: 18.0"
                 vim.wo.relativenumber = true
             end
 
