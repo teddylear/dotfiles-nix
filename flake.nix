a0d087d18a89237e6ae3b41785bc66cefaf4a2f8
teddylear <20077627+teddylear@users.noreply.github.com>
2024-06-25 19:38:14 -0400

    feat: Move nix home-manager and nixpkgs back to latest stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Example kickstart Nix development setup.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     darwin = {
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,7 +10,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
