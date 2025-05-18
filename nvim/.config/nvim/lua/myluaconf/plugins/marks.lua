303a6ffbaa382b94cea601b2322eff450e4eb281
teddylear <20077627+teddylear@users.noreply.github.com>
2025-05-17 20:04:03 -0400

    feat: Added plugin for marks

---
+++ nvim/.config/nvim/lua/myluaconf/plugins/marks.lua
@@ -0,0 +1,13 @@
+return {
+    "chentoast/marks.nvim",
+    event = "VeryLazy",
+    opts = {
+        mappings = {
+            set_next = "<leader>m",
+            next = "mn",
+            preview = "m:",
+            set_bookmark0 = "m0",
+            prev = false -- pass false to disable only this default mapping
+        }
+    },
+}
