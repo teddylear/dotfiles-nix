8a621edd1c2a560b72b35a7a95e0d75493814a44
teddylear <20077627+teddylear@users.noreply.github.com>
2025-06-11 19:11:49 -0400

    Added ftplugin/markdown.lua for markdown specific keymaps - <leader>s to toggle spell check in normal mode - <leader>rd to RenderMarkdown plugin toggle in normal mode

---
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>s", ":setlocal spell!<CR>", { buffer = true, desc = "Toggle spell check" })
+vim.keymap.set("n", "<leader>rd", ":RenderMarkdown buf_toggle<CR>", { buffer = true, desc = "Toggle RenderMarkdown" })
