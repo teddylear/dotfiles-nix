72a96a416ca3e6c1f9495a81cd8b1355a18427b1
teddylear <20077627+teddylear@users.noreply.github.com>
2025-07-01 10:30:03 -0400

    Add back hardtime.nvim with default off and '<leader>ht' in normal mode to toggle.

---
+++ nvim/.config/nvim/lua/myluaconf/plugins/hardtime.lua
@@ -0,0 +1,23 @@
+return {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+    config = function()
+        local hardtime = require("hardtime")
+        hardtime.setup({})
+        hardtime.disable()
+
+        local enabled = false
+        vim.keymap.set("n", "<leader>ht", function()
+            enabled = not enabled
+            if enabled then
+                hardtime.enable()
+                vim.notify("Hardtime enabled", vim.log.levels.INFO)
+            else
+                hardtime.disable()
+                vim.notify("Hardtime disabled", vim.log.levels.INFO)
+            end
+        end, { desc = "Toggle hardtime.nvim" })
+    end,
+}
