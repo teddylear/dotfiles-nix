ca66a41fe3c676b15f59848039d7f09b91a312dd
teddylear <20077627+teddylear@users.noreply.github.com>
2025-07-31 22:31:29 -0400

    Moved `jd` to shell abbreviation for 'jj diff --git | delta --features=chameleon` to show jj diffs via delta.
    Removed `jd` as an alias for `jj desc` ad it would config with above.

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -178,7 +178,6 @@
     '';
 
     shellAliases = {
-      jd = "jj desc";
       jf = "jj git fetch";
       jn = "jj new";
       jp = "jj git push";
@@ -236,6 +235,7 @@
       gcm = "git commit -m";
       ll = "eza -l -g -a --icons";
       tmuxsession = "fish $HOME/tmux-session.fish";
+      jd = "jj diff --git | delta --features=chameleon";
     };
   };
 
@@ -312,6 +312,42 @@
     # TODO: fix this later once I'm ready for linux
     # terminal = if isDarwin then "screen-256color" else "xterm-256color";
   };
+
+  home.file.".deltarc".text = ''
+    [delta]
+    features = chameleon
+    side-by-side = true
+
+    [chameleon]
+    dark = true
+    line-numbers = true
+    side-by-side = true
+    keep-plus-minus-markers = true
+    syntax-theme = "Nord"
+    file-style = "#434C5E bold"
+    file-decoration-style = "#434C5E ul"
+    file-added-label = "[+]"
+    file-copied-label = "[==]"
+    file-modified-label = "[*]"
+    file-removed-label = "[-]"
+    file-renamed-label = "[->]"
+    hunk-header-style = "omit"
+    line-numbers-left-format = " {nm:>1} │"
+    line-numbers-left-style = "red"
+    line-numbers-right-format = " {np:>1} │"
+    line-numbers-right-style = "green"
+    line-numbers-minus-style = "red italic black"
+    line-numbers-plus-style = "green italic black"
+    line-numbers-zero-style = "#434C5E italic"
+    minus-style = "bold red"
+    minus-emph-style = "bold red"
+    plus-style = "bold green"
+    plus-emph-style = "bold green"
+    zero-style = "syntax"
+    blame-code-style = "syntax"
+    blame-format = "{author:<18} ({commit:>7}) {timestamp:^12} "
+    blame-palette = "#2E3440 #3B4252 #434C5E #4C566A"
+  '';
 
   home.file.".jjconfig.toml".text = ''
     [user]
