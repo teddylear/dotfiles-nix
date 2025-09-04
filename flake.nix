fe198f7cab9a1c14caf737af3ae9ac9eca1cc8ff
teddylear <20077627+teddylear@users.noreply.github.com>
2025-09-04 16:32:10 -0400

    Updated neovim to latest version ~ `0.11.4`

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # Then use command to get hash and update below when commented out:
     # jq '.nodes."nixpkgs-unstable".locked.rev' < flake.lock
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/c87b95e25065c028d31a94f06a62927d18763fdf";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/d0fc30899600b9b3466ddb260fd83deb486c32f1";
 
     darwin = {
       inputs.nixpkgs.follows = "nixpkgs";
