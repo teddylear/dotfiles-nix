42ee7c0cee04b5756c7b4e3a9da6f26001ec9654
teddylear <20077627+teddylear@users.noreply.github.com>
2026-03-17 17:34:39 -0400

    feat(nvim): added fff.nvim picker with file and grep keymaps

---
+++ nvim/.config/nvim/lua/myluaconf/plugins/fff.lua
@@ -0,0 +1,42 @@
+return {
+    "dmtrKovalenko/fff.nvim",
+    build = function()
+        -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+        -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+        require("fff.download").download_or_build_binary()
+    end,
+    -- if you are using nixos
+    -- build = "nix run .#release",
+    opts = { -- (optional)
+        debug = {
+            enabled = false,
+            show_scores = false,
+        },
+    },
+    -- No need to lazy-load with lazy.nvim.
+    -- This plugin initializes itself lazily.
+    lazy = false,
+    keys = {
+        {
+            "ff", -- try it if you didn't it is a banger keybinding for a picker
+            function()
+                require("fff").find_files()
+            end,
+            desc = "FFFind files",
+        },
+        {
+            "fg",
+            function()
+                require("fff").live_grep()
+            end,
+            desc = "LiFFFe grep",
+        },
+        {
+            "fc",
+            function()
+                require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+            end,
+            desc = "Search current word",
+        },
+    },
+}
