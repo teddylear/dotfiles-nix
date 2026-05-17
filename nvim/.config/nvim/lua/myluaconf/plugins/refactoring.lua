2346dfdc11b12cba2bdd9679380ca102733b8a70
teddylear <20077627+teddylear@users.noreply.github.com>
2026-05-17 16:26:58 -0400

    chore: Ran `just fmt`

--- nvim/.config/nvim/lua/myluaconf/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/myluaconf/plugins/refactoring.lua
@@ -2,44 +2,44 @@
 -- local map = vim.api.nvim_set_keymap
 
 -- return {
-    -- "ThePrimeagen/refactoring.nvim",
-    -- dependencies = {
-        -- "nvim-lua/plenary.nvim",
-        -- "nvim-treesitter/nvim-treesitter",
-    -- },
-    -- config = function()
-        -- require("refactoring").setup({
-            -- prompt_func_return_type = {
-                -- go = true,
-            -- },
-            -- prompt_func_param_type = {
-                -- go = true,
-            -- },
-        -- })
+-- "ThePrimeagen/refactoring.nvim",
+-- dependencies = {
+-- "nvim-lua/plenary.nvim",
+-- "nvim-treesitter/nvim-treesitter",
+-- },
+-- config = function()
+-- require("refactoring").setup({
+-- prompt_func_return_type = {
+-- go = true,
+-- },
+-- prompt_func_param_type = {
+-- go = true,
+-- },
+-- })
 
-        -- map(
-            -- "v",
-            -- "<Leader>ef",
-            -- [[<Esc><Cmd>lua require("refactoring").refactor("Extract Function")<CR>]],
-            -- {
-                -- noremap = true,
-                -- silent = true,
-                -- expr = false,
-                -- desc = "Refactoring extract function",
-            -- }
-        -- )
+-- map(
+-- "v",
+-- "<Leader>ef",
+-- [[<Esc><Cmd>lua require("refactoring").refactor("Extract Function")<CR>]],
+-- {
+-- noremap = true,
+-- silent = true,
+-- expr = false,
+-- desc = "Refactoring extract function",
+-- }
+-- )
 
-        -- map(
-            -- "v",
-            -- "<Leader>ev",
-            -- [[<Esc><Cmd>lua require("refactoring").refactor("Extract Variable")<CR>]],
-            -- {
-                -- noremap = true,
-                -- silent = true,
-                -- expr = false,
-                -- desc = "Refactoring extract variable",
-            -- }
-        -- )
-    -- end,
+-- map(
+-- "v",
+-- "<Leader>ev",
+-- [[<Esc><Cmd>lua require("refactoring").refactor("Extract Variable")<CR>]],
+-- {
+-- noremap = true,
+-- silent = true,
+-- expr = false,
+-- desc = "Refactoring extract variable",
+-- }
+-- )
+-- end,
 -- }
 return {}
