8c81b877df5e3cffb26c7b94a3cff9eb10aa31e9
teddylear <20077627+teddylear@users.noreply.github.com>
2026-05-18 10:37:31 -0400

    fix(nvim): Cleaned up old nvim plugins and resolved treesitter issue with upgrade to `main` branch

--- nvim/.config/nvim/lua/myluaconf/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/myluaconf/plugins/treesitter.lua
@@ -7,13 +7,20 @@ return {
         config = function()
             local ts = require("nvim-treesitter")
 
-            ts.setup({})
+            -- The rewritten main branch expects an explicit install_dir if you want
+            -- its parser/query install location prepended to runtimepath.
+            -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/README.md#setup
+            ts.setup({
+                install_dir = vim.fn.stdpath("data") .. "/site",
+            })
 
             local group = vim.api.nvim_create_augroup(
                 "myluaconf-treesitter-highlight",
                 { clear = true }
             )
 
+            -- On main, highlighting is provided by Neovim and enabled per filetype.
+            -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/README.md#highlighting
             vim.api.nvim_create_autocmd("FileType", {
                 group = group,
                 callback = function(args)
